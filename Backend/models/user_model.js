ebab8cefc755151f2785978ea6bb96075a0039f2
Harshit Joshi <joshiharshit291@gmail.com>
2025-09-12 17:17:58 +0530

    server ,db,models

--- Backend/models/user_model.js
+++ Backend/models/user_model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const { Schema } = mongoose;
+
 const userSchema = new Schema({
     username: {
     type: String,
@@ -15,7 +17,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-})
+});
 
 const User = mongoose.model('User', userSchema);
 
--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,19 @@ const port = process.env.PORT || 5000;
 app.use(cors()); // middleware
 app.use(express.json()); // Replaces bodyParser.json()
 
+app.post('/api/reg', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+})
+
+
+
 connectDB();
 
 app.listen(port, () => {
